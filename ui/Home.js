325ed3615e7b110ad998170243365054979850f6
Jekko07 <jerichojamesranara@gmail.com>
2024-09-17 13:41:58 +0800

    added protected routes

--- ui/Home.js
+++ ui/Home.js
@@ -2,30 +2,8 @@ import React from 'react';
 import { Wallet } from './Wallet.js';
 import { ContactForm } from './ContactForm.js';
 import { ContactList } from './ContactList.js';
-import { useLoggedUser } from 'meteor/quave:logged-user-react';
-import { Loading } from './components/Loading.js';
-import { RoutePaths } from './RoutePaths.js';
-import { useNavigate } from 'react-router-dom';
 
 export const Home = () => {
-  const { loggedUser, isLoadingLoggedUser } = useLoggedUser();
-  const navigate = useNavigate();
-
-  if (isLoadingLoggedUser) {
-    return <Loading />;
-  }
-
-  if (!loggedUser) {
-    return (
-      <div className="flex flex-col items-center p-12">
-        <div>Welcome!</div>
-        <div>
-          Please <a className="text-indigo-800 cursor-pointer" onClick={() => navigate(RoutePaths.ACCESS)}>sign-up</a>
-        </div>
-      </div>
-    );
-  }
-
   return (
     <>
       <Wallet />
--- ui/Router.js
+++ ui/Router.js
@@ -4,12 +4,27 @@ import { Home } from './Home';
 import { NotFound } from './NotFound';
 import { Access } from './Access';
 import { RoutePaths } from './RoutePaths';
-
+import { LoggedUserOnly } from './components/LoggedUserOnly';
+import { AnonymousOnly } from './components/AnonymousOnly';
 
 export const AppRoutes = () => (
   <Routes>
-    <Route path={RoutePaths.HOME} element={<Home />} />
-    <Route path={RoutePaths.ACCESS} element={<Access />} />
+    <Route
+      path={RoutePaths.HOME}
+      element={
+        <LoggedUserOnly>
+          <Home />
+        </LoggedUserOnly>
+      }
+    />
+    <Route
+      path={RoutePaths.ACCESS}
+      element={
+        <AnonymousOnly>
+          <Access />
+        </AnonymousOnly>
+      }
+    />
     <Route path="*" element={<NotFound />} />
   </Routes>
 );
